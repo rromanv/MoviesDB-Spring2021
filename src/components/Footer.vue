fb79ced74d4270440b14f105a0cc04bdd1346a89
Rodolfo Roman <rromanv@gmail.com>
2021-04-23 01:36:09 -0500

    Footer Layout

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,1 +1,29 @@
-<template>Footer</template>
+<template>
+  <footer class="bg-coolGray-900">
+    <div
+      class="container flex items-center justify-between px-2 py-4 mx-auto space-x-4 space-y-2 text-coolGray-200"
+    >
+      <div class="text-xs">
+        Favicon made by
+        <a
+          href="https://www.flaticon.com/authors/prosymbols"
+          title="Prosymbols"
+          class="text-red-500"
+          >Prosymbols</a
+        >
+        from
+        <a
+          href="https://www.flaticon.com/"
+          title="Flaticon"
+          class="text-red-500"
+          >www.flaticon.com</a
+        >
+      </div>
+      <img :src="tmdbLogo" alt="TMDB Logo" class="w-12" />
+    </div>
+  </footer>
+</template>
+
+<script setup>
+  import tmdbLogo from '~/assets/tmdb.svg'
+</script>
